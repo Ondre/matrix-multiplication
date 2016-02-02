efed0d25a7dc0a60a3b7f22207cc58364b9e7e8b
Andrey Penner <snipe94@mail.ru>
2016-02-02 19:53:18 +0600

    Minor fix of the code style

--- src/main/java/com/epam/ap/MatrixMultiplicator.java
+++ src/main/java/com/epam/ap/MatrixMultiplicator.java
@@ -1,7 +1,7 @@
 package com.epam.ap;
 
 public class MatrixMultiplicator {
-    public static Matrix multiply(Matrix m1, Matrix m2){
+    public static Matrix multiply(Matrix m1, Matrix m2) {
 
         if (m1.getCols() != m2.getRows()) {
             System.err.println("Unable to multiply. Number of columns of the first matrix must be equal to the number of rows of the second matrix.");
@@ -13,7 +13,7 @@ public class MatrixMultiplicator {
         for (int i = 0; i < m1.getRows(); i++) {
             for (int j = 0; j < m2.getCols(); j++) {
                 for (int k = 0; k < m2.getRows(); k++) {
-                    result[i][j] += (m1.get(i,k) * m2.get(k,j));
+                    result[i][j] += m1.get(i, k) * m2.get(k, j);
                 }
             }
         }
--- src/main/java/com/epam/ap/Runner.java
+++ src/main/java/com/epam/ap/Runner.java
@@ -3,22 +3,24 @@ package com.epam.ap;
 public class Runner {
     public static void main(String[] args) {
         double[][] a = new double[][]
-                {       {1,2,3},
-                        {4,5,6},
-                        {4,5,6.5},
-                        {1,3,5}
+                {       {1, 2, 3},
+                        {4, 5, 6},
+                        {4, 5, 6.5},
+                        {1, 3, 5}
                 };
         double[][] b = new double[][]
-                {       {1,2,3,5},
-                        {4,5,6,4},
-                        {4,5,6.5,8}
+                {       {1, 2, 3, 5},
+                        {4, 5, 6, 4},
+                        {4, 5, 6.5, 8}
                 };
 
-        Matrix A = new Matrix(a);
-        Matrix B = new Matrix(b);
-        System.out.println(A);
-        System.out.println(B);
-        System.out.println(MatrixMultiplicator.multiply(A,B));
+        Matrix aMatrix = new Matrix(a);
+        Matrix bMatrix = new Matrix(b);
+
+        System.out.println(aMatrix);
+        System.out.println(bMatrix);
+
+        System.out.println(MatrixMultiplicator.multiply(aMatrix, bMatrix));
 
     }
 }
